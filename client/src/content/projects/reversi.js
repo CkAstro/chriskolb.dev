3958eda2962675a2de932d1db0af076fc2785929
Chris <cekolb.astro@gmail.com>
2022-06-19 20:24:54 -0400

    init

---
+++ client/src/content/projects/reversi.js
@@ -0,0 +1,13 @@
+import ContentPage from '../../components/contentpage';
+
+const reversi = {
+   title: 'Reversi',
+   making: 'React / Express / MongoDB / Websockets',
+   image: require('./reversi.png'),
+   description: 'Online multi-player board game. Supports multiple games and replays.',
+   pages: [
+      <ContentPage>Hello World this is a content page</ContentPage>
+   ],
+}
+
+export default reversi;
