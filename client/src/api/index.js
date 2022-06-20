35e80fb054a7e4dfcb38982a8cfa109c3b6cefe1
Chris <cekolb.astro@gmail.com>
2022-06-20 19:54:35 -0400

    init

---
+++ client/src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = window.location.host.includes('localhost')
+   ? 'http://localhost:3001/api'
+   : '/api'
+;
+
+const getCSMData = (mrto, vwind, vrto) => {
+   const request = axios.get(`${baseUrl}/csm/${mrto}/${vwind}/${vrto}`);
+   return request.then(response => response.data);
+}
+
+export { getCSMData };
+export default { getCSMData };
