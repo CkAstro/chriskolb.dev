ef5878e0f97be86ce8aaf198946e79fe86c8f56b
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-20 13:00:40 -0400

    moved content page logic to modal context

--- client/src/components/contentmodal/index.js
+++ client/src/components/contentmodal/index.js
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import { useDisplay } from '../../contexts/display';
 import { useModal } from '../../contexts/modal';
 import style from './contentmodal.module.css';
 
 const ContentModal = () => {
-   const [ page, setPage ] = useState(0);
-   const { modalProps, closeModal } = useModal();
+   const { modalProps, toNextPage, toPrevPage, closeModal } = useModal();
    const { setIsEnabled } = useDisplay();
 
    const handleClick = event => event.stopPropagation();
@@ -15,34 +13,19 @@ const ContentModal = () => {
       setIsEnabled(true);
    }
 
-   const nextPage = () => {
-      const newPage = page+1;
-      newPage > modalProps.content.length-1
-         ? setPage(0)
-         : setPage(newPage)
-      ;
-   }
-   const prevPage = () => {
-      const newPage = page-1;
-      newPage < 0
-         ? setPage(modalProps.content.length-1)
-         : setPage(newPage)
-      ;
-   }
-
    return (
       <div className={`${style.modalContainer} ${modalProps.isActive ? style.active : null}`} 
          onScroll={closeModal}
          onClick={handleClose}
       >
          <div className={style.modal} onClick={handleClick}>
-            {modalProps.content && modalProps.content[page] ? modalProps.content[page] : null}
+            {modalProps.content && modalProps.content[modalProps.page] ? modalProps.content[modalProps.page] : null}
             <div className={style.topBar}>
                <div className={style.closeButton} onClick={handleClose}>&times;</div>
             </div>
-            <div className={style.pageNumber}>{`${page+1} / ${modalProps.content ? modalProps.content.length : 1}`}</div>
-            <div onClick={nextPage} className={style.nextButton}>&#x27A4;</div>
-            <div onClick={prevPage} className={style.prevButton}>&#x27A4;</div>
+            <div className={style.pageNumber}>{`${modalProps.page+1} / ${modalProps.content ? modalProps.content.length : 1}`}</div>
+            <div onClick={toNextPage} className={style.nextButton}>&#x27A4;</div>
+            <div onClick={toPrevPage} className={style.prevButton}>&#x27A4;</div>
          </div>
       </div>
    );
--- client/src/contexts/modal.js
+++ client/src/contexts/modal.js
@@ -3,6 +3,7 @@ import { useState, useContext, createContext } from 'react';
 const defaultProps = {
    content: null,
    isActive: false,
+   page: 0,
 }
 
 const ModalContext = createContext();
@@ -24,7 +25,22 @@ const useModal = () => {
       const updatedProps = {
          content: content,
          isActive: true,
+         page: 0,
       }
+      setModalProps(updatedProps);
+   }
+
+   const toNextPage = () => {
+      const updatedProps = { ...modalProps }
+      const newPage = modalProps.page+1;
+      updatedProps.page = newPage > modalProps.content.length-1 ? 0 : newPage;
+      setModalProps(updatedProps);
+   }
+
+   const toPrevPage = () => {
+      const updatedProps = { ...modalProps }
+      const newPage = modalProps.page-1;
+      updatedProps.page = newPage < 0 ? modalProps.content.length-1 : newPage;
       setModalProps(updatedProps);
    }
 
@@ -39,6 +55,8 @@ const useModal = () => {
    return {
       modalProps,
       setModalContent,
+      toNextPage,
+      toPrevPage,
       closeModal,
    }
 }
