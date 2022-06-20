71ecf9f7a7fe51019800b99643ce4676308c77e6
Chris <cekolb.astro@gmail.com>
2022-06-20 10:33:02 -0400

    content test

--- client/src/content/projects/reversi.js
+++ client/src/content/projects/reversi.js
@@ -1,4 +1,6 @@
 import ContentPage from '../../components/contentpage';
+import FancyButton from '../../components/fancybutton';
+import style from './reversi.module.css';
 
 const reversi = {
    title: 'Reversi',
@@ -6,7 +8,19 @@ const reversi = {
    image: require('./reversi.png'),
    description: 'Online multi-player board game. Supports multiple games and replays.',
    pages: [
-      <ContentPage>Hello World this is a content page</ContentPage>
+      <ContentPage>
+         <div className={style.imageContainer}>
+            <img src={require('./reversi.png')}/>
+         </div>
+         <div className={style.contentContainer}>
+            <h1>Reversi (aka Othello)</h1>
+            <p>Play your friends in this online multi-player board game. Supports multiple games and a replay mode.</p>
+            <p>Front end created with React and uses a Websocket to communicate with the server.</p>
+            <p>Back end uses Node.js with Express to handle API requests, server/client communication via Websocket, and all game logic.</p>
+            <a href='https://reversi.chriskolb.dev'><FancyButton>Play Now</FancyButton></a>
+         </div>
+      </ContentPage>,
+      <ContentPage>Yo dawg it's page 2</ContentPage>,
    ],
 }
 
