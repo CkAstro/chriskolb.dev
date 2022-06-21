3e48dc0f7387a9608063db750f8839291b36e986
Chris <cekolb.astro@gmail.com>
2022-06-21 10:10:04 -0400

    api to fetch csm files

--- server/src/web.js
+++ server/src/web.js
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 
 import { PORT } from './config/index.js';
 'use strict';
@@ -10,6 +11,29 @@ const app = express();
 app.use(compression());
 app.use(cors());
 app.use(express.json());
+
+// ----- log ----- //
+const requestLogger = (request, response, next) => {
+   const message = {
+      method: request.method,
+      path: request.path,
+      body: request.body,
+   }
+   console.log('received api call:', message);
+   next();
+}
+app.use(requestLogger);
+
+// ----- RESTful ----- //
+app.get('/api/csm/:mrto/:vwind/:vrto', (req, res) => {
+   const { mrto, vwind, vrto } = req.params;
+   fs.readFile(`./src/data/csm/50-${mrto}-${vwind}-${vrto}-lri.json`, (err, data) => {
+      if (err) console.log(err);
+      res.json(JSON.parse(data));
+   });
+   // const data = require(`./data/csm/50-${mrto}-${vwind}-${vrto}-lri.json`);
+   // res.json(data);
+});
 
 // ----- static serving ----- //
 app.use(express.static('../client/build'));     // NOTE: this MUST come after API requests
