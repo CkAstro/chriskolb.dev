621f8963550a7908f41e5087fc0eae9d0d5bca34
Chris <cekolb.astro@gmail.com>
2022-06-21 10:12:27 -0400

    content update

--- client/src/content/projects/datavis.js
+++ client/src/content/projects/datavis.js
@@ -1,4 +1,6 @@
 import ContentPage from '../../components/contentpage';
+import FancyButton from '../../components/fancybutton';
+import style from './datavis.module.css';
 
 const datavis = {
    title: 'DataVis',
@@ -6,8 +8,28 @@ const datavis = {
    image: require('./datavis.png'),
    description: 'Online multi-player board game. Supports multiple games and replays.',
    pages: [
-      <ContentPage>Hello World this is a content page</ContentPage>,
-      <ContentPage>Yo dawg it's page 2</ContentPage>,
+      <ContentPage>
+         <div className={style.imageContainer}>
+            <img src={require('./datavis.png')}/>
+         </div>
+         <div className={`${style.contentContainer}`}>
+            <h1>DataVis</h1>
+
+            <p>View volumetric scalar data from your browser. This web-app allows the user to upload and view their own research data with support for multiple formats and resolutions.</p>
+
+            <p>The front-end is created in React and uses a custom WebGL/GLSL implementation to quickly render volumetric data mapped to planar and spherical shapes, or shown as an iso-surface (a surface of constant value). The back-end runs on Node.js with Express and will process incomming datasets to add an easily-renderable data format to the API response.</p>
+
+            <a href='https://datavis.chriskolb.dev'><FancyButton.Style2>View App</FancyButton.Style2></a>
+         </div>
+      </ContentPage>,
+
+      <ContentPage>
+         <div className={style.contentContainer}>
+            <h1>Using the App</h1>
+
+            <p>Coming soon.</p>
+         </div>
+      </ContentPage>,
    ],
 }
 
