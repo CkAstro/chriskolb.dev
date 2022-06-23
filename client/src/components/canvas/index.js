45b52ea4b06272ea2bf5d5514c2b6082d40d4c52
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-23 12:00:56 -0400

    GL2Canvas import/export

--- client/src/components/canvas/index.js
+++ client/src/components/canvas/index.js
@@ -1,4 +1,5 @@
-import InteractiveCanvas from "./interactivecanvas";
+import InteractiveCanvas from './interactivecanvas';
+import GL2Canvas from './gl2canvas'
 
-export { InteractiveCanvas };
-export default { InteractiveCanvas };
+export { InteractiveCanvas, GL2Canvas, };
+export default { InteractiveCanvas, GL2Canvas, };
