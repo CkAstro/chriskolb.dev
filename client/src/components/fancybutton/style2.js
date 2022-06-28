44c57957fff9e3507af8acb4624d547cd7b19bf1
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-28 16:09:26 -0400

    touch controls + layout updated for main site

--- client/src/components/fancybutton/style2.js
+++ client/src/components/fancybutton/style2.js
@@ -4,10 +4,13 @@ import style from './fancybutton.module.css';
 const FancyButton = ({ children }) => {
    const [ isMouseOver, setIsMouseOver ] = useState(false);
 
+   const handleTouch = event => event.stopPropagation();
+
    return (
       <div className={style.buttonContainer} 
          onMouseEnter={() => setIsMouseOver(true)} 
          onMouseLeave={() => setIsMouseOver(false)}
+         onTouchEnd={handleTouch}
       >
          <div className={`${style.normalButton2} ${isMouseOver ? style.hover : null}`}>{children}</div>
          <div className={`${style.hoverButton2} ${isMouseOver ? style.hover : null}`}>{children}</div>
