fe67dd88f187ca55dc2da5e2fc69ba9aa6a9501c
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-29 15:04:17 -0400

    section container now uses relative position

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -1,23 +1,7 @@
-// import { useState, useEffect } from 'react';
 import { Element } from 'react-scroll';
-// import { useDisplay } from '../../contexts/display';
-// import ContentSquare from '../contentsquare';
 import style from './section.module.css';
 
 const Section = ({ navId, styleAdjust, children }) => {
-   // const [ isHidden, setIsHidden ] = useState(false);
-   // const { isEnabled, setIsEnabled } = useDisplay();
-
-   // useEffect(() => {
-   //    if (isEnabled) return setIsHidden(false);
-   //    return setIsHidden(true);
-   // }, [isEnabled]);
-
-   // const buildContent = () => {
-   //    return content.map((item, ind) => {
-   //       return <ContentSquare info={item} key={ind} isHidden={isHidden}/>;
-   //    });
-   // }
 
    return <Element id={navId.toLowerCase()} style={styleAdjust} className={style.sectionContainer}>
       <div className={`contentContainer ${style.section}`}>
