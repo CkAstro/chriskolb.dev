3979fa2e42a701b308173f332deeb775e32f6c7b
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-29 15:04:44 -0400

    init; likely will not use this particular figure

---
+++ client/src/content/intro/texhelper.js
@@ -0,0 +1,118 @@
+// NOTE: this is a custom texture helper implementation
+//    - loadDataFromImage scales texture data for emission calculation
+
+// NOTE: on page switch, a new gl instance is created and this must recreate
+//    all textures; attempted to save texture buffer and recreate from buffer
+//    data, but this is no faster than just recreating from image
+
+class TextureHelper {
+   constructor() {
+      this.textures = {
+         data: [],
+         cmap: [],
+      }
+      this.glInstance = null;
+      this.isInit = false;
+      this.baseUrl = window.location.host.includes('localhost')
+      ? 'http://localhost:3004/api/img'
+      : (window.location.host.includes('192')
+         ? 'http://192.168.3.105:3004/api/img'
+         : '/api/img')
+   ;
+   }
+
+   init(gl) {
+      this.glInstance = gl;
+      this.textures = {
+         data: [],
+         cmap: [],
+      }
+
+      // scaling process in data texture will lock up browser, delay added for CSS transition
+      setTimeout(() => {
+         this.textures.cmap = this.textures.cmap.concat(this.loadCmapFromImage('instability_cmap.png'));
+         for (let i=0; i<41; i++) {
+            const file = i < 10 
+               ? `instability_64__0${i}.png`
+               : `instability_64__${i}.png`
+            ;
+            this.textures.data = this.textures.data.concat(this.loadDataFromImage(file));
+         }
+      }, 750);
+
+      this.isInit = true;
+   }
+
+   loadCmapFromImage(img) {
+      const gl = this.glInstance;
+      const texture = gl.createTexture();
+
+      const image = new Image;
+      image.onload = () => {
+         gl.bindTexture(gl.TEXTURE_2D, texture);
+         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      }
+      image.crossOrigin = '';
+      image.src = `${this.baseUrl}/${img}`;
+      return texture;
+   }
+
+   loadDataFromImage(img) {
+      const gl = this.glInstance;
+      const texture = gl.createTexture();
+
+      const image = new Image;
+      image.onload = () => {
+         const canvas = document.createElement('canvas');
+         canvas.width = image.width;
+         canvas.height = image.height;
+         const ctx = canvas.getContext('2d');
+         ctx.drawImage(image, 0, 0);
+
+         let volRes;
+         if (image.width === image.height) {
+            const guess = image.width**(2./3.);
+            if (Math.abs(guess - Math.round(guess)) < 0.1) volRes = Math.round(guess);
+         }
+         if (!volRes) return console.log('non-pow(2)**3 image dimensions not yet accounted for');
+
+         // init 3D texture
+         gl.bindTexture(gl.TEXTURE_3D, texture);
+         gl.texStorage3D(gl.TEXTURE_3D, 1, gl.RGBA32F, volRes, volRes, volRes);
+         gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+         gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+         // loop through image and fill texture
+         let sliceCount = Math.floor(volRes**0.5);
+         if (volRes % sliceCount) sliceCount += 1;    // add one if not perfect square
+         const dataBuffer = new Float32Array(4*volRes*volRes*volRes);
+         for (let k=0; k<volRes; k++) {
+            // grab each slice, scale, and add to dataBuffer
+            const x = k % sliceCount;
+            const y = Math.floor(k/sliceCount);
+            const img = ctx.getImageData(x*volRes, y*volRes, volRes, volRes);
+   
+            for (let j=0; j<volRes; j++) {
+               for (let i=0; i<volRes; i++) {
+                  for (let n=0; n<3; n++) {
+                     // NOTE, this is (j,i,k) instead of (k,i,j) to re-orient incorrect images
+                     dataBuffer[4*j*volRes*volRes+4*i*volRes+4*k+n] = img.data[4*j*volRes+4*i+n] / 255.0;
+                  }
+               }
+            }
+         }
+         // add to texture layer by layer
+         gl.texSubImage3D(gl.TEXTURE_3D, 0, 0, 0, 0, volRes, volRes, volRes, gl.RGBA, gl.FLOAT, dataBuffer);
+      }
+      image.crossOrigin = '';
+      image.src = `${this.baseUrl}/${img}`;
+      return texture;
+   }
+}
+
+export default new TextureHelper;
