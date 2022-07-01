8df1424f561d57a9dd64e099bddd7cd8aef01bc4
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-01 12:20:09 -0400

    'contentContainer' style only active if 'isContent' is passed; minor rework

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -1,10 +1,9 @@
 import { Element } from 'react-scroll';
 import style from './section.module.css';
 
-const Section = ({ navId, styleAdjust, children }) => {
-
+const Section = ({ navId, styleAdjust, isContent, children }) => {
    return <Element id={navId.toLowerCase()} style={styleAdjust} className={style.sectionContainer}>
-      <div className={`contentContainer ${style.section}`}>
+      <div className={`${isContent ? 'contentContainer' : ''} ${style.section}`}>
          {children}
       </div>
    </Element>;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ModalProvider } from './contexts/modal';
 import { DisplayProvider } from './contexts/display';
-import { MousePositionProvider, useMousePosition } from './contexts/mouseposition';
+import { MousePositionProvider } from './contexts/mouseposition';
 import ReactDOM from 'react-dom/client';
 import Section from './components/section';
 import Header from './components/header';
@@ -16,22 +16,17 @@ import './index.css';      // global impor
 
 const Main = () => {
    const [ scrollValue, setScrollValue ] = useState(0);
-   const { setMousePosition } = useMousePosition();
 
    const handleScroll = event => setScrollValue(event.target.scrollTop);
-   const handleMouseMove = event => {
-      const { clientX, clientY } = event;
-      setMousePosition({ x: clientX, y: clientY });
-   }
 
-   return <div id='mainContainer' className='mainContainer' onScroll={handleScroll} onMouseMove={handleMouseMove}>
+   return <div id='mainContainer' className='mainContainer' onScroll={handleScroll}>
       <ContentModal/>
       <Navbar scrollValue={scrollValue}/>
-      <Section navId='home' styleAdjust={{height: '100vh', background: '#1e1e1e'}}><Header/></Section>
-      <Section navId='about' styleAdjust={{minHeight: '100vh', background: 'linear-gradient(#1e1e1e, #0b3ce1)'}}><About/></Section>
-      <Section navId='projects' styleAdjust={{background: 'linear-gradient(#0b3ce1, #78e9ff)'}}><Projects/></Section>
-      <Section navId='research' styleAdjust={{background: 'linear-gradient(#78e9ff, #fff)'}}><Research/></Section>
-      <Section navId='contact' styleAdjust={{minHeight: '100vh'}}><Contact/></Section>
+      <Section navId='home' styleAdjust={{height: '100vh', background: '#1e1e1e', paddingTop: '0'}}><Header/></Section>
+      <Section navId='about' isContent styleAdjust={{minHeight: '100vh', background: 'linear-gradient(#1e1e1e, #0b3ce1)'}}><About/></Section>
+      <Section navId='projects' isContent styleAdjust={{background: 'linear-gradient(#0b3ce1, #78e9ff)'}}><Projects/></Section>
+      <Section navId='research' isContent styleAdjust={{background: 'linear-gradient(#78e9ff, #fff)'}}><Research/></Section>
+      <Section navId='contact' isContent styleAdjust={{minHeight: '100vh'}}><Contact/></Section>
       
    </div>;
 }
