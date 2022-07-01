2bad9ec77bfe60f658f735355b66a39ee33a4e4e
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-01 12:28:34 -0400

    size dependent on screen

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,15 +1,22 @@
-// import { Element } from 'react-scroll';
+import { useMousePosition } from '../../contexts/mouseposition';
 import NucleoDisplay from '../nucleodisplay';
 import style from './header.module.css';
 
 const Header = () => {
-   return <>
-      <NucleoDisplay/>
-      <div className={style.headerContainer}>
-         <h2 style={{zIndex: '10', color: '#999'}}>Full Stack Development + Computational Astrophysics</h2>
-         <h1 style={{zIndex: '10', color: '#999'}}>Gene Richardson</h1>
+   const { setMousePosition } = useMousePosition();
+
+   const handleMouseMove = event => {
+      const { clientX, clientY } = event;
+      setMousePosition({ x: clientX, y: clientY });
+   }
+
+   return <div onMouseMove={handleMouseMove}>
+      <div className={`noselect ${style.headerContainer}`}>
+         <h1>Christopher Kolb</h1>
+         <h2>Full Stack Development + Computational Astrophysics</h2>
       </div>
-   </>;
+      <NucleoDisplay/>
+   </div>;
 }
 
 export default Header;
