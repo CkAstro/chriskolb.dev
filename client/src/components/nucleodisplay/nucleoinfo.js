fec8cec070c79af01e4d2b284a0ac2fadbff1273
Chris <cekolb.astro@gmail.com>
2022-07-02 09:59:00 -0400

    stylized 'learn more' and added link; adjusted color for stable elements to match

--- client/src/components/nucleodisplay/nucleoinfo.js
+++ client/src/components/nucleodisplay/nucleoinfo.js
@@ -1,9 +1,11 @@
 import style from './nucleodisplay.module.css';
 
 const NucleoInfo = ({ setStyle }) => {
-   return <div style={setStyle} className={style.learnMore}>
-      <p>nuclear isotopes</p>
-      <p>learn more</p>
+   return <div style={setStyle}  className={style.learnMore}>
+      <a href='https://en.wikipedia.org/wiki/Table_of_nuclides' target='_blank'>
+         <p>nuclear isotopes</p>
+         <p>learn more</p>
+      </a>
    </div>;
 }
 
