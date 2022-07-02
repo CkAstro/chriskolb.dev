214c54e2d074bb3901388a7bca50d5528dd9d75e
Chris <cekolb.astro@gmail.com>
2022-07-02 16:18:14 -0400

    page is functional

--- client/src/content/about/index.js
+++ client/src/content/about/index.js
@@ -1,4 +1,6 @@
-
+import { useRef, memo } from 'react';
+import useIsVisible from '../../hooks/useisvisible';
+import style from './about.module.css';
 // idea: loop a bunch of super easy equations
 //    and then one really complex one
 
@@ -16,12 +18,77 @@
 // "a b c triangle"
 // 'my' wind equation
 
-const About = () => {
-   return <>
+const IntroContainer = () => {
+   const divRef = useRef(null);
+   const isVisible = useIsVisible(divRef, true);
+
+   return <div ref={divRef} className={`${style.introContainer} ${isVisible ? style.active : null}`}>
       <h1>Hi, I'm Chris.</h1>
-      {/* <p>I love math and science.</p>
-      <p>I also love programming.</p> */}
-   </>;
+      <p style={{'--delay': '1.0s'}}>I love math and physics.</p>
+      <p style={{'--delay': '2.5s'}}>I also love designing awesome stuff,</p>
+      <div className={style.test}>
+         <p style={{'--delay': '4.0s'}}>like responsive sites,</p>
+         <p style={{'--delay': '5.0s'}}>interactive figures,</p>
+         <p style={{'--delay': '6.0s'}}>and complex simulations.</p>
+      </div>
+   </div>;
 }
 
-export default About;
+const About = () => {
+   return <div className={style.aboutContainer}>
+      <IntroContainer/>
+
+      <div className={style.flexColumns}>
+         <Card>
+            <h1>{'Languages'}</h1>
+            <CardItem percent='90'>Javascript</CardItem>
+            <CardItem percent='90'>HTML5 / CSS3 / SVG</CardItem>
+            <CardItem percent='80'>Python</CardItem>
+            <CardItem percent='80'>OpenGL / WebGL / GLSL</CardItem>
+            <CardItem percent='75'>FORTRAN</CardItem>
+            <CardItem percent='75'>Bash</CardItem>
+            <CardItem percent='75'>LaTeX</CardItem>
+            <CardItem percent='50'>C / C++</CardItem>
+            <CardItem percent='50'>PHP</CardItem>
+            <CardItem percent='25'>Java</CardItem>
+         </Card>
+         <Card>
+            <h1>{'Frameworks & APIs'}</h1>
+            <CardItem percent='90'>React</CardItem>
+            <CardItem percent='90'>NodeJS / Express</CardItem>
+            <CardItem percent='90'>RESTful</CardItem>
+            <CardItem percent='90'>OpenMP / MPI</CardItem>
+            <CardItem percent='50'>Redux</CardItem>
+            <CardItem percent='50'>OpenACC</CardItem>
+         </Card>
+         <Card>
+            <h1>{'Tools & Software'}</h1>
+            <CardItem percent='95'>NumPy / SciPy</CardItem>
+            <CardItem percent='90'>UNIX / Linux / Windows</CardItem>
+            <CardItem percent='90'>Microsoft Office Suite</CardItem>
+            <CardItem percent='80'>GiT</CardItem>
+            <CardItem percent='75'>PANDAS</CardItem>
+            <CardItem percent='75'>GIMP / Photoshop</CardItem>
+            <CardItem percent='50'>Blender</CardItem>
+         </Card>
+      </div>
+   </div>;
+}
+
+const Card = ({ children }) => {
+   const divRef = useRef(null);
+   const isVisible = useIsVisible(divRef, true);
+
+   return <div ref={divRef} className={`${style.card} ${isVisible ? style.active : null}`}>
+      {children}
+   </div>
+};
+
+const CardItem = ({ percent, children }) => {
+   return <div className={style.cardItem}>
+      <div>{children}</div>
+      <div style={{'--width': `${percent}%`}} className={style.percentContainer}/>
+   </div>;
+}
+
+export default memo(About);
