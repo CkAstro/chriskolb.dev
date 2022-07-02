0ebe59c7d883d0d8f14a7cf0c2abc86ca6938fbf
Chris <cekolb.astro@gmail.com>
2022-07-02 16:26:04 -0400

    stylizing

--- client/src/components/nucleodisplay/nucleoinfo.js
+++ client/src/components/nucleodisplay/nucleoinfo.js
@@ -4,7 +4,7 @@ const NucleoInfo = ({ setStyle }) => {
    return <div style={setStyle}  className={style.learnMore}>
       <a href='https://en.wikipedia.org/wiki/Table_of_nuclides' target='_blank'>
          <p>nuclear isotopes</p>
-         <p>learn more</p>
+         <p>&gt; learn more</p>
       </a>
    </div>;
 }
