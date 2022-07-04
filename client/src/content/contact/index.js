daa9841e79fc895e4d21ae696e290d01bac4070f
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-04 10:12:10 -0400

    added footer

--- client/src/content/contact/index.js
+++ client/src/content/contact/index.js
@@ -1,5 +1,6 @@
 import ContactForm from "../../components/contactform";
 import style from './contact.module.css';
+import Icons from '../../components/icons';
 
 const Contact = () => {
    return <>
@@ -8,7 +9,18 @@ const Contact = () => {
          <p>Have a question or want to work together?</p>
       </div>
       <ContactForm/>
+      <Footer/>
    </>;
+}
+
+const Footer = () => {
+
+   return <div className={style.footer}>
+      <a href='https://github.com/CkAstro' target='_blank'><Icons.GitHub size={24}/></a>
+      <a href='https://www.linkedin.com/in/christopher-kolb-071334232/' target='_blank'><Icons.LinkedIn size={24}/></a>
+      <div className={style.email}>astro.cekolb@gmail.com</div>
+      <div className={style.copyright}>Copyright Christopher Kolb, 2022</div>
+   </div>;
 }
 
 export default Contact;
