f331d4ea50b5b20af4f34f90a89764050f412079
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-04 13:53:16 -0400

    added 'next' button to move to portfolio section

--- client/src/content/about/introcontainer.js
+++ client/src/content/about/introcontainer.js
@@ -1,4 +1,6 @@
 import { useRef } from 'react';
+import { Link } from 'react-scroll';
+import Icons from '../../components/icons';
 import useIsVisible from '../../hooks/useisvisible';
 import style from './about.module.css';
 
@@ -11,11 +13,15 @@ const IntroContainer = () => {
       <p style={{'--delay': '4.0s'}}>I love math and physics.</p>
       <p style={{'--delay': '5.5s'}}>I also love designing awesome stuff,</p>
       <div className={style.test}>
-         <p style={{'--delay': '7.0s'}}>like responsive sites,</p>
-         <p style={{'--delay': '8.0s'}}>interactive figures,</p>
-         <p style={{'--delay': '9.0s'}}>and complex simulations.</p>
+         <p style={{'--delay': '6.5s'}}>like responsive sites,</p>
+         <p style={{'--delay': '7.25s'}}>interactive figures,</p>
+         <p style={{'--delay': '8.0s'}}>and complex simulations.</p>
+      </div>
+      <div style={{'--delay': '9s'}} className={style.linkIcon}>
+         <Link smooth spy to='projects' containerId='mainContainer'>
+               <Icons.DownArrow fill='black' size='36' setStyle={{cursor: 'pointer'}}/>
+         </Link>
       </div>
    </div>;
 }
-
 export default IntroContainer;
