68dc02a5a5cd625a6e2701b92bd642aeba6980e1
Christopher Kolb <CkAstro.git@gmail.com>
2022-07-14 15:24:26 -0400

    large performance update for isotope chart

--- client/src/components/nucleodisplay/index.js
+++ client/src/components/nucleodisplay/index.js
@@ -1,28 +1,46 @@
 import { useEffect, useState, useRef } from 'react';
 import { useMousePosition } from '../../contexts/mouseposition';
-import { nucleoChart } from './utils';
-import buildChart from './buildchart';
+import { nucleoChart, useSquareSize } from './utils';
+import buildDisplayChart from './builddisplaychart';
 import organizeChart from './organizechart';
+import Spotlight from './spotlight';
+import buildStableSquares from './buildstablesquares';
 import style from './nucleodisplay.module.css';
 
 const NucleoDisplay = () => {
-   const [ chart, setChart ] = useState(null);
+   const [ displayChart, setDisplayChart ] = useState(null);
    const [ organizedChart, setOrganizedChart ] = useState(null);
+   const [ stableSquares, setStableSquares ] = useState([]);
    const { mousePosition } = useMousePosition();
+
+   const { squareSize } = useSquareSize();
 
    const divRef = useRef(null);
 
+   // organize data on init
    useEffect(() => {
-      organizeChart(nucleoChart, setOrganizedChart);
-   }, [window.innerWidth, window.innerHeight]);
+      const chart = organizeChart(nucleoChart)
+      setOrganizedChart(chart);
+   }, []);
+
+   // then build display chart
+   useEffect(() => {
+      const chart = buildDisplayChart(organizedChart, mousePosition, squareSize, divRef);
+      setDisplayChart(chart);
+   }, [mousePosition, organizedChart, squareSize]);
 
    useEffect(() => {
-      buildChart(organizedChart, mousePosition, divRef, setChart);
-   }, [mousePosition, organizedChart]);
+      const squares = buildStableSquares(organizedChart, squareSize, divRef);
+      setStableSquares(squares);
+   }, [organizedChart, window.innerWidth, window.innerHeight, squareSize]);
 
-   return <div ref={divRef} className={style.nucleodisplayContainer}>
-      {chart}
-   </div>;
+   return (
+      <div ref={divRef} className={style.nucleodisplayContainer}>
+         <div className={style.flexFill}/>
+         <Spotlight squares={stableSquares} divRef={divRef}/>
+         {displayChart}
+      </div>
+   );
 }
 
 export default NucleoDisplay;
